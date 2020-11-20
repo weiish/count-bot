2a1e570d49f16d3ae0b8f43250ddf032254e41c2
Wei Shi <shixwei@gmail.com>
2020-11-19 20:55:19 -0600

    Change db connection to a pool, which should reconnect as needed

--- database/db.js
+++ database/db.js
@@ -1,18 +1,34 @@
 const mysql = require('mysql')
 const keys = require('../config/keys')
 const util = require('util');
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+    connectionLimit: 5,
     host: keys.DB_HOST,
     user: keys.DB_USER,
     password: keys.DB_PASS,
     database: keys.DB_NAME,
     charset: 'utf8mb4'
 })
-const query = util.promisify(connection.query).bind(connection);
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Database Connected')
-})
+// Ping database to check for common exception errors.
+pool.getConnection((err, connection) => {
+    if (err) {
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.error('Database connection was closed.')
+      }
+      if (err.code === 'ER_CON_COUNT_ERROR') {
+        console.error('Database has too many connections.')
+      }
+      if (err.code === 'ECONNREFUSED') {
+        console.error('Database connection was refused.')
+      }
+    }
+  
+    if (connection) connection.release()
+  
+    return
+  })
 
-module.exports = { connection, query}
+const query = util.promisify(pool.query).bind(pool);
+
+module.exports = { pool, query}
